14152087919ef18c710b7b5a99e5e214ff5a13e1
rhkd4560 <rhkd4560@naver.com>
2019-03-28 19:48:46 +0900

    one To one -> one To Many mapping

---
+++ Day-3/Project_TDL/src/main/java/com/gwangmin/tdl/domain/User.java
@@ -0,0 +1,45 @@
+package com.gwangmin.tdl.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+@Setter
+public class User implements Serializable {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idx;
+
+    @Column
+    private String id;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<ToDoList> toDoLists = new ArrayList<>();
+
+    public void add(ToDoList toDoList){
+        toDoList.setUser(this); //owner
+        getToDoLists().add(toDoList);
+    }
+
+    @Builder
+    public User(String id, String password, String email, List<ToDoList> toDoLists) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.toDoLists = toDoLists;
+    }
+}
---
+++ Day-3/Project_TDL/src/main/java/com/gwangmin/tdl/service/TodoListService.java
@@ -0,0 +1,33 @@
+package com.gwangmin.tdl.service;
+
+import com.gwangmin.tdl.domain.ToDoList;
+import com.gwangmin.tdl.domain.User;
+import com.gwangmin.tdl.repository.ToDoListRepository;
+import com.gwangmin.tdl.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoListService {
+    private final ToDoListRepository toDoListRepository;
+    private final UserRepository userRepository;
+
+
+    public TodoListService(ToDoListRepository toDoListRepository, UserRepository userRepository){
+        this.toDoListRepository = toDoListRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<ToDoList> findList(User user) {
+        return toDoListRepository.findAllByUserOrderByIdxAsc(user);
+    }
+
+    public User findUser(String email){
+        return userRepository.findByemail(email);
+    }
+
+}
